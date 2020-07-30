8791a6a709768e7594978a4b21d9df9bf0bfdefb
Admin <>
2020-07-30 14:48:20 +0530

    Added tdd file for unittest.

--- test/tdd.py
+++ test/tdd.py
@@ -2,19 +2,19 @@ import unittest
 
 
 class TestSum(unittest.TestCase):
-    """ Checking the various sum of the numbers. """
+    """ Checking the various sum of the numbers."""
 
-    def test_sum_list(self):  # defining the condition to pass the list method.
+    def test_sum_list(self):
+        """defining the condition to pass the list method. """
         self.assertEqual(sum([3, 4, 5]), 12, 'Should be 12.')
 
-    def test_sum_tuple(self):  # defining the condition to fail the tuple method.
+    def test_sum_tuple(self):
+        """ defining the condition to fail the tuple method."""
         self.assertEqual(sum((2, 3, 6)), 12, 'Should be 12 as well')
 
     def test_sum_str(self):
+        """ defining the condition to fail the mixed method."""
         values = ['True', '1', 2]
-        # values = {'a':1, 'b':2, 'c':3}
-        # values = sum(values[values])
-        # sum(values[values]) = sum.values(values)
         self.assertEqual(sum([values]), 4, 'Should be 4 only.')
 
 
