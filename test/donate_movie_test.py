3711bae4dd034feea9e684cd2b2221a4609428c1
Admin <>
2020-07-30 13:57:06 +0530

    Added tdd file for unittest.

--- test/donate_movie_test.py
+++ test/donate_movie_test.py
@@ -1,26 +1,40 @@
-import unittest
-from unittest import TestCase
+''' Trying to driven into the Unit TestCases '''
 
+import unittest
+# from unittest import TestCase
 # from setuptools.extension import Library
 
 
-class Library(object):
+class Library:
+    """ Initializing the variables """
+
+    catalogue = None
+
     def __init__(self):
         self.catalogue = []
 
-    def donate(self, movie):
+    def donate(self, movie):  # pylist: missing-function-docstring
         self.catalogue.append(movie)
 
 
-class DonateMovieTest(unittest.TestCase):   #Test Case for Donate Movie.
+class Movie:
+    """ Creating movie class """
+    STAT = 25
 
-    def test_donate_movie(self):
+    def func(self):
+        """ Passing the method """
+        pass
+
+
+class DonateMovieTest(unittest.TestCase):
+    """ creating test class for movie """
+
+    def test_donate_movie(self):  # created another function
+        """ creating test class for movie """
         movie = Movie()
         library = Library()
         library.donate(movie)
         self.assertTrue(movie in Library.catalogue)
-
-
 
 
 if __name__ == '__main__':
